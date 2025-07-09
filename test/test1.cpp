dccb14f4db82d3764aaa4085a4b50b9c0da24360
ricky <1120845871@qq.com>
2025-07-09 22:59:45 +0800

    兼容CLion

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,90 +1,73 @@
 #include <gtest/gtest.h>
 
-/*
-    [两数之和]
-
-    给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出
-    和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-
-    你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
-
-    你可以按任意顺序返回答案。
-
-    示例 1：
-    输入：nums = [2,7,11,15], target = 9
-    输出：[0,1]
-    解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
-
-    示例 2：
-    输入：nums = [3,2,4], target = 6
-    输出：[1,2]
-
-    示例 3：
-    输入：nums = [3,3], target = 6
-    输出：[0,1]
-
-
-    提示：
-        2 <= nums.length <= 104
-        -109 <= nums[i] <= 109
-        -109 <= target <= 109
-        只会存在一个有效答案
-
-    进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
-
-*/
-
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (hashtable.find(target - nums[i]) != hashtable.end()) {
-                return {hashtable[target - nums[i]], i};
+    int longestConsecutive(vector<int>& nums) {
+        int len = 1;
+        int max_len = 0;
+        sort(nums.begin(), nums.end());
+        if (nums.size() > 1)
+        {
+            for (unsigned int i = 1; i < nums.size(); ++i)
+            {
+                int dif = nums[i] - nums[i - 1];
+                if (dif == 1)
+                {
+                    len++;
+                }
+                else if (dif == 0)
+                {
+
+                }
+                else
+                {
+                    len = 1;
+                }
+                max_len = max(max_len, len);
             }
-            hashtable[nums[i]] = i;
+            return max_len;
         }
-        return {};
+        else if (nums.size() == 1)
+            return 1;
+        else
+            return 0;
+
     }
 };
 
-TEST(LeetCode1, SumofTwoNumbers1)
+TEST(LeetCode, example1)
 {
     Solution sol;
-
-    vector<int> nums = {2, 7, 11, 15};
-    int         target = 9;
-    vector<int> expect = {0, 1};
-
-    EXPECT_EQ(sol.twoSum(nums, target), expect);
+    vector<int> nums = {100,4,200,1,3,2};
+    int expect = 4;
+    EXPECT_EQ(sol.longestConsecutive(nums), expect);
 }
 
-
-TEST(LeetCode1, SumofTwoNumbers2)
+TEST(LeetCode, example2)
 {
     Solution sol;
-
-    vector<int> nums = {3, 2, 4};
-    int         target = 6;
-    vector<int> expect = {1, 2};
-
-    EXPECT_EQ(sol.twoSum(nums, target), expect);
+    vector<int> nums = {0,3,7,2,5,8,4,6,0,1};
+    int expect = 9;
+    EXPECT_EQ(sol.longestConsecutive(nums), expect);
 }
 
-TEST(LeetCode1, SumofTwoNumbers3)
+TEST(LeetCode, example3)
 {
     Solution sol;
-
-    vector<int> nums = {3, 3};
-    int         target = 6;
-    vector<int> expect = {0, 1};
-
-    EXPECT_EQ(sol.twoSum(nums, target), expect);
+    vector<int> nums = {1,0,1,2};
+    int expect = 3;
+    EXPECT_EQ(sol.longestConsecutive(nums), expect);
 }
 
+TEST(LeetCode, example4)
+{
+    Solution sol;
+    vector<int> nums = {0,0};
+    int expect = 1;
+    EXPECT_EQ(sol.longestConsecutive(nums), expect);
+}
